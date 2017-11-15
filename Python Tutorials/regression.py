f625c5155b4ce478a5c7cb29c08551927269a20b
Daniel Gormly <dgormly@me.com>
2017-11-15 19:48:12 +1000

    Added new file that creates it's own linear regression line using the sum of squares

--- Python Tutorials/regression.py
+++ Python Tutorials/regression.py
@@ -3,7 +3,7 @@ from sklearn import linear_model
 import matplotlib.pyplot as plt
 
 #read data
-dataframe = pd.read_fwf('brain_body.txt')
+dataframe = pd.read_fwf('resources/brain_body.txt')
 x_values = dataframe[['Brain']]
 y_values = dataframe[['Body']]
 
